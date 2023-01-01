034e0b63c21944cb27ca137eeb76a3133f911227
Leo Banos Garcia <bsleo625@gmail.com>
2023-01-01 16:25:21 -0700

    Adds seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@department_1 = Department.create({name: "Shoes", level: 2})
+@department_2 = Department.create({name: "Purses", level: 2})
+@department_3 = Department.create({name: "Appliances", level: 1})
+@employee_1 = Employee.create({name: "Jeff", level: 2})
+@employee_2 = Employee.create({name: "Margaret", level: 2})
+@employee_3 = Employee.create({name: "Carla", level: 2})
+@employee_4 = Employee.create({name: "Juanita", level: 1})
+@employee_5 = Employee.create({name: "Facundo", level: 1})
+@employee_6 = Employee.create({name: "Meghan", level: 1})
